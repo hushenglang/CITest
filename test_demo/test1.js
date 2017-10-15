7bb73d2fa807960cd8dbfff2b79834413203cefe
hushenglang <hushenglang@gmail.com>
2017-10-16 07:46:50 +0800

    update

--- test_demo/test1.js
+++ test_demo/test1.js
@@ -4,8 +4,12 @@ const expect = require('chai').expect
 
 describe('test_demo demo', () => {
     describe('"up"', () => {
-        it('should export a function', () => {
+        it('should be 2', () => {
             expect(2).to.equal(2);
         });
+
+    it('should be 3', () => {
+        expect(3).to.equal(3);
+});
     });
 });
