a3e32e443ac70443886d0d5f1a548b3ca27cda33
hushenglang <hushenglang@gmail.com>
2017-10-15 21:14:25 +0800

    add test

---
+++ test_demo/test1.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const expect = require('chai').expect
+
+describe('test_demo demo', () => {
+    describe('"up"', () => {
+        it('should export a function', () => {
+            expect(2).to.equal(2);
+        });
+    });
+});
